09cd87c12210162c2744d86f71efed93ee3bffd3
Max Lyth <maxlyth@users.noreply.github.com>
2026-07-29 13:33:27 +0000

    chore(release): allocate versionCode 515

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3IntegrationWaveOwnsVersionCode514() {
+    @Test fun rc3IntegrationWaveOwnsVersionCode515() {
         assertEquals("0.9.6-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(514, BuildConfig.VERSION_CODE)
+        assertEquals(515, BuildConfig.VERSION_CODE)
     }
 }
