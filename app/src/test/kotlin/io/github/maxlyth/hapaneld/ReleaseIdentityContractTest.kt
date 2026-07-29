c950132fce6836ed3bf873601d2b94f3f4049fca
Max Lyth <maxlyth@users.noreply.github.com>
2026-07-29 12:27:44 +0000

    test: advance rc3 integration identity to versionCode 514

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3IntegrationWaveOwnsVersionCode513() {
+    @Test fun rc3IntegrationWaveOwnsVersionCode514() {
         assertEquals("0.9.6-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(513, BuildConfig.VERSION_CODE)
+        assertEquals(514, BuildConfig.VERSION_CODE)
     }
 }
