9abe443dfd0c49e250ebd25f15bfb9b9ad125533
Max Lyth <maxlyth@users.noreply.github.com>
2026-07-31 18:14:23 +0000

    chore: allocate rc5 integration build 526

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc5CandidateOwnsVersionCode525() {
+    @Test fun rc5CandidateOwnsVersionCode526() {
         assertEquals("0.9.6-rc5", BuildConfig.VERSION_NAME)
-        assertEquals(525, BuildConfig.VERSION_CODE)
+        assertEquals(526, BuildConfig.VERSION_CODE)
     }
 }
