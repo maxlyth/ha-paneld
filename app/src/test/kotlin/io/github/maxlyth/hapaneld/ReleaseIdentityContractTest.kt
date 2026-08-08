86d1767031f1540d61da7b93a8a7ab1f1689328d
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-08 10:40:53 +0000

    chore: allocate vc540 for integration wave

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc1CandidateOwnsVersionCode539() {
+    @Test fun rc1CandidateOwnsVersionCode540() {
         assertEquals("0.9.7-rc1", BuildConfig.VERSION_NAME)
-        assertEquals(539, BuildConfig.VERSION_CODE)
+        assertEquals(540, BuildConfig.VERSION_CODE)
     }
 }
