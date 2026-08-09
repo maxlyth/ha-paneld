8a390485d306546d961afdb3640352d9a4568a69
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-09 17:45:13 +0000

    chore: allocate version code 547

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc1CandidateOwnsVersionCode546() {
+    @Test fun rc1CandidateOwnsVersionCode547() {
         assertEquals("0.9.7-rc1", BuildConfig.VERSION_NAME)
-        assertEquals(546, BuildConfig.VERSION_CODE)
+        assertEquals(547, BuildConfig.VERSION_CODE)
     }
 }
