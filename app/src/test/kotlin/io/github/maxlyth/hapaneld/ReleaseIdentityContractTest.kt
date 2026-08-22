e926223db968fc7767479a5482b6c27dbab4a041
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-22 18:07:38 +0000

    chore: allocate rc2 integration build 594

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc2CandidateOwnsVersionCode593() {
+    @Test fun rc2CandidateOwnsVersionCode594() {
         assertEquals("0.9.7-rc2", BuildConfig.VERSION_NAME)
-        assertEquals(593, BuildConfig.VERSION_CODE)
+        assertEquals(594, BuildConfig.VERSION_CODE)
     }
 }
