b1957c5630ad940335dddd85c961a91a961961cc
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-25 13:39:32 +0000

    test: track rc2 build 609 identity

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc2CandidateOwnsVersionCode608() {
+    @Test fun rc2CandidateOwnsVersionCode609() {
         assertEquals("0.9.7-rc2", BuildConfig.VERSION_NAME)
-        assertEquals(608, BuildConfig.VERSION_CODE)
+        assertEquals(609, BuildConfig.VERSION_CODE)
     }
 }
