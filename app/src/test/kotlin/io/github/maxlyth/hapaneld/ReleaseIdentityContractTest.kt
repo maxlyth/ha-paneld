3d5553a6934f11b5bd8594e20d6b8b7bfcb4d38b
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-26 12:50:43 +0000

    build: set 0.9.7-rc2 version code 612

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc2CandidateOwnsVersionCode611() {
+    @Test fun rc2CandidateOwnsVersionCode612() {
         assertEquals("0.9.7-rc2", BuildConfig.VERSION_NAME)
-        assertEquals(611, BuildConfig.VERSION_CODE)
+        assertEquals(612, BuildConfig.VERSION_CODE)
     }
 }
