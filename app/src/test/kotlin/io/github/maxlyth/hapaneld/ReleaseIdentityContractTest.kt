66c92962faa248ecf12cd225a3de807222829995
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-27 14:08:15 +0000

    chore: allocate rc3 integration build 617

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3CandidateOwnsVersionCode615() {
+    @Test fun rc3CandidateOwnsVersionCode617() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(615, BuildConfig.VERSION_CODE)
+        assertEquals(617, BuildConfig.VERSION_CODE)
     }
 }
