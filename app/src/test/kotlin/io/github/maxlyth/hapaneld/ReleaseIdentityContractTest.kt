7055ad3f62e6ec643d00a310d369740b1ae04ab6
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-30 20:57:35 +0000

    test: align release identity with versionCode 658

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -6,6 +6,6 @@ import org.junit.Test
 class ReleaseIdentityContractTest {
     @Test fun rc3CandidateOwnsVersionCode657() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(657, BuildConfig.VERSION_CODE)
+        assertEquals(658, BuildConfig.VERSION_CODE)
     }
 }
