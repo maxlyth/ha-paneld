73ece94d76281efcca6c4ca8db7790f71c2a57d8
Max Lyth <maxlyth@users.noreply.github.com>
2026-08-30 18:46:14 +0000

    test: align RC3 release identity with vc656

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3CandidateOwnsVersionCode649() {
+    @Test fun rc3CandidateOwnsVersionCode656() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(655, BuildConfig.VERSION_CODE)
+        assertEquals(656, BuildConfig.VERSION_CODE)
     }
 }
