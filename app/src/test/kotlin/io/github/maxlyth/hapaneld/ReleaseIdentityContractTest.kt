506d5e13442388d0bad206dce2595ead88c6838d
Max Lyth <maxlyth@users.noreply.github.com>
2026-09-01 20:35:19 +0000

    chore: allocate vc666 integration identity

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3CandidateOwnsVersionCode665() {
+    @Test fun rc3CandidateOwnsVersionCode666() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(665, BuildConfig.VERSION_CODE)
+        assertEquals(666, BuildConfig.VERSION_CODE)
     }
 }
