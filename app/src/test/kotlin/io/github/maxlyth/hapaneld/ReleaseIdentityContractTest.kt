c33875f7a7a3ecdd1e4bdd71e8dfd42fa275f804
Max Lyth <maxlyth@users.noreply.github.com>
2026-09-02 17:37:59 +0000

    test: bind the rc3 wave to versionCode 671

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3CandidateOwnsVersionCode669() {
+    @Test fun rc3CandidateOwnsVersionCode671() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(669, BuildConfig.VERSION_CODE)
+        assertEquals(671, BuildConfig.VERSION_CODE)
     }
 }
