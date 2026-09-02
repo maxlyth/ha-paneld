762b1650c5c4dbc8fb1214752ee0684dd2806f75
Max Lyth <maxlyth@users.noreply.github.com>
2026-09-02 13:04:50 +0000

    build: allocate multilingual acceptance identity

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3CandidateOwnsVersionCode668() {
+    @Test fun rc3CandidateOwnsVersionCode669() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(668, BuildConfig.VERSION_CODE)
+        assertEquals(669, BuildConfig.VERSION_CODE)
     }
 }
