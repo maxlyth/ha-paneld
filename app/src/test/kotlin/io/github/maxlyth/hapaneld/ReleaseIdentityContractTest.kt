9a2b3e268caaa1afd25eb706b064f5b9eaf6b1d3
Max Lyth <maxlyth@users.noreply.github.com>
2026-09-03 00:49:17 +0000

    chore: allocate versionCode 679

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3CandidateOwnsVersionCode678() {
+    @Test fun rc3CandidateOwnsVersionCode679() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(678, BuildConfig.VERSION_CODE)
+        assertEquals(679, BuildConfig.VERSION_CODE)
     }
 }
