6354311463364cfcd8dee13ea569b5848490b3d0
Max Lyth <maxlyth@users.noreply.github.com>
2026-09-03 16:59:53 +0000

    test: bind rc3 identity to vc683

--- app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
+++ app/src/test/kotlin/io/github/maxlyth/hapaneld/ReleaseIdentityContractTest.kt
@@ -4,8 +4,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ReleaseIdentityContractTest {
-    @Test fun rc3CandidateOwnsVersionCode682() {
+    @Test fun rc3CandidateOwnsVersionCode683() {
         assertEquals("0.9.7-rc3", BuildConfig.VERSION_NAME)
-        assertEquals(682, BuildConfig.VERSION_CODE)
+        assertEquals(683, BuildConfig.VERSION_CODE)
     }
 }
